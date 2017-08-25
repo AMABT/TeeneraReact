590bda5ab74fec39ffb64807a7f733a0e9986168
Alecu Marian <a.marian.alexandru@gmail.com>
2017-08-25 10:26:59 +0300

    Make first slide height equal with window height

--- src/components/home-header.js
+++ src/components/home-header.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Container, Segment, Button, Icon, Header, Visibility} from 'semantic-ui-react';
+import {Segment, Button, Icon, Header, Visibility, Grid} from 'semantic-ui-react';
 import {DefaultMenu, FollowingMenu} from '../components/header-menu';
 
 export default class HomeHeader extends Component {
@@ -18,14 +18,16 @@ export default class HomeHeader extends Component {
         <FollowingMenu visible={this.state.followingVisible}/>
         <Segment inverted vertical textAlign="center" className="masthead">
           <DefaultMenu/>
-          <Container text>
-            <Header as="h1" inverted>Imagine-a-Company</Header>
-            <h2>Do whatever you want when you want to.</h2>
-            <Button as="div" size="huge" primary>
-              Get Started
-              <Icon name="arrow right"/>
-            </Button>
-          </Container>
+          <Grid verticalAlign="middle">
+            <Grid.Column>
+              <Header as="h1" inverted>Imagine-a-Company</Header>
+              <h2>Do whatever you want when you want to.</h2>
+              <Button as="div" size="huge" primary>
+                Get Started
+                <Icon name="arrow right"/>
+              </Button>
+            </Grid.Column>
+          </Grid>
         </Segment>
       </Visibility>
     )
