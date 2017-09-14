3eea9da80e2a570b17e219013874407d5cecb24c
Alecu Marian <a.marian.alexandru@gmail.com>
2017-09-14 12:04:23 +0300

    Refactor menu to use functions instead of classes and remove redundant comments from index.html

--- src/components/menu.js
+++ src/components/menu.js
@@ -11,61 +11,52 @@ const menuItems = [
   {url: '/', text: 'Home'},
 ].map((item, index) => (<Menu.Item as={Link} to={item.url} key={index}>{item.text}</Menu.Item>))
 
-class Default extends Component {
+const Default = props => {
 
-  render() {
+  const {homeHeaderContent, contentVisible} = props
+  const loginParam = contentVisible !== 'LOGIN' ? 'LOGIN' : null;
+  const signupParam = contentVisible !== 'SIGNUP' ? 'SIGNUP' : null;
 
-    const {homeHeaderContent, contentVisible} = this.props
-    const loginParam = contentVisible !== 'LOGIN' ? 'LOGIN' : null;
-    const signupParam = contentVisible !== 'SIGNUP' ? 'SIGNUP' : null;
-
-    return (
-      <Container>
-        <Menu size="large" secondary inverted className="default-menu">
-          <Menu.Item className="toc">
-            <Icon name="sidebar"></Icon>
-          </Menu.Item>
-          {menuItems}
-          <Menu.Item className="right">
-            <Button inverted
-                    onClick={() => homeHeaderContent(loginParam)}
-                    active={contentVisible === 'LOGIN'}>Login</Button>
-            <Button inverted
-                    onClick={() => homeHeaderContent(signupParam)}
-                    active={contentVisible === 'SIGNUP'}>Sign Up</Button>
-          </Menu.Item>
-        </Menu>
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <Menu size="large" secondary inverted className="default-menu">
+        <Menu.Item className="toc">
+          <Icon name="sidebar"></Icon>
+        </Menu.Item>
+        {menuItems}
+        <Menu.Item className="right">
+          <Button inverted
+                  onClick={() => homeHeaderContent(loginParam)}
+                  active={contentVisible === 'LOGIN'}>Login</Button>
+          <Button inverted
+                  onClick={() => homeHeaderContent(signupParam)}
+                  active={contentVisible === 'SIGNUP'}>Sign Up</Button>
+        </Menu.Item>
+      </Menu>
+    </Container>
+  )
 }
 
 export const DefaultMenu = connect(
-  state => {
-    return {
-      contentVisible: state.homeContent.contentVisible
-    }
-  },
+  state => ({contentVisible: state.homeContent.contentVisible}),
   actions
 )(Default);
 
-export class FollowingMenu extends Component {
+export const FollowingMenu = props => {
 
-  render() {
-    const {visible} = this.props;
+  const {visible} = props;
 
-    return (
-      <Transition animation="fade" visible={visible} duration={300}>
-        <Menu size="large" fixed="top">
-          <Container>
-            {menuItems}
-            <div className="right menu">
-              <Menu.Item><Button>Log in</Button></Menu.Item>
-              <Menu.Item><Button primary>Sign up</Button></Menu.Item>
-            </div>
-          </Container>
-        </Menu>
-      </Transition>
-    )
-  }
+  return (
+    <Transition animation="fade" visible={visible} duration={300}>
+      <Menu size="large" fixed="top">
+        <Container>
+          {menuItems}
+          <div className="right menu">
+            <Menu.Item><Button>Log in</Button></Menu.Item>
+            <Menu.Item><Button primary>Sign up</Button></Menu.Item>
+          </div>
+        </Container>
+      </Menu>
+    </Transition>
+  )
 }
